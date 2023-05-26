d489252c01c744dfbf1af51273f98b1989326b1f
atrevorMatovu <atrevormatovu@gmail.com>
2023-05-26 05:00:18 -0700

    Dang

--- unint.c
+++ unint.c
@@ -5,16 +5,17 @@
  *
  * Return: 0 always
  */
-void begin()
+void begin(void)
 {
 	pid_t shellPID = getpid();
+
 	SH_INTERACTIVE = isatty(STDIB_FILENO);
 
 	if (SH_INTERACTIVE)
 	{
 		do {
 			kill(shellPID, SIGTTIN);
-		} while(tcgetpgrp(STDIN_FILENO) != (shellPGID = getpgrp()));
+		} while (tcgetpgrp(STDIN_FILENO) != (shellPGID = getpgrp()));
 
 		struct sigaction childAction;
 		struct sigaction intAction;
@@ -24,7 +25,7 @@ void begin()
 
 		sigaction(SIGCHLD, &childAction, 0);
 		sigaction(SIGINT, &intAction, 0);
-		setpgid(shellPID, shellPID);setpgid(shellPID, shellPID);
+		setpgid(shellPID, shellPID);
 		shellPGID = getpgrp();
 
 		if (shellPID != shellPGID)
@@ -34,7 +35,7 @@ void begin()
 		}
 		tcsetpgrp(STDIN_FILENO, shellPGID);
 		tcgetattr(STDIN_FILENO, &shellAttributes);
-		currentDirectory = (char*)calloc(1024, sizeof(char));
+		currentDir = (char *) calloc(1024, sizeof(char));
 	}
 	else
 	{
@@ -44,4 +45,47 @@ void begin()
 }
 
 /**
+ * uninteractive - kinda like offline mode
  *
+ * Return: nothing.
+ */
+void uninteractive(void)
+{
+	char **tr, *hn;
+	int lash = -1;
+
+	while (1)
+	{
+		hn = readstr();
+		if (hn == NULL)
+		{
+			break;
+		}
+		tr = divline(hn);
+		lash = execar(tr);
+		free(hn);
+		free(tr);
+		if (lash == 0)
+		{
+			break;
+		}
+	}
+}
+
+/**
+ * execar - to execute commands
+ * @args: pointer
+ *
+ * Return: always 0
+ */
+int execar(char **args)
+{
+	if (args[0] != NULL && strcmp(args[0], "exit") == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
